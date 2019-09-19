c221d6931e5dadd85849653b9c8191c954b237b4
morgancluff <morgan.cluff@colorado.edu>
2019-09-19 16:47:27 -0600

    added documentation

--- lab3_script.sh
+++ lab3_script.sh
@@ -11,22 +11,22 @@ read file
 echo "Enter a regular expression: "
 read regx
 
-#2
+#2 serach specified file for specified regex
 egrep $regx $file
 
 #3 search for 3 digits, a dash, 3 digits, a dash, then 4 digits
 egrep -c '[[:digit:]]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
 
 #4
-#4.1
+#4.1 search for text followed by '@', more text, '.', and more text
 egrep -c '.+@.+\..+' regex_practice.txt
 #4.2 search for "303-" followed by 3 digits, a dash, and 4 digits
 egrep -o '303-[[:digit:]]{3}-[[:digit:]]{4}' regex_practice.txt
 #4.3 search for anything ending in "@geocities.com"
 egrep -o '.+@geocities.com' regex_practice.txt > email_results.txt
 
-#5
+#5 stage created file
 git add email_results.txt
 
-#6
+#6 commit to local repository
 git commit -m "Commit from shell script"
